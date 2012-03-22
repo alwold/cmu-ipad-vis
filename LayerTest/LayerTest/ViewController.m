42ccbe0e30236d8f4dff7dd6f068b8f27c9d4d27
Al Wold <alwold@gmail.com>
2012-03-22 10:26:07 -0700

    a little core animation fun

---
+++ LayerTest/LayerTest/ViewController.m
@@ -0,0 +1,67 @@
+//
+//  ViewController.m
+//  LayerTest
+//
+//  Created by Al Wold on 3/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation ViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+	CALayer *layer = [[CALayer alloc] init];
+	layer.bounds = CGRectMake(0, 0, 100, 100);
+	layer.position = CGPointMake(60, 60);
+	layer.backgroundColor = [UIColor redColor].CGColor;
+	
+	[self.view.layer addSublayer:layer];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	return YES;
+}
+
+@end
